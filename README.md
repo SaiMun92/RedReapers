dc6607c220c1e7885b5285188f54ab2448f06c75
icecoolcat <icecoolcat@users.noreply.github.com>
2015-10-12 14:02:17 +0800

    test commit message

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # RedReapers
 You are not supposed to see this yet. 
+
+Client
+event, helpers, templates
+
+Server
+methods, and the Meteor.publish statement
+
+the PlayersList = new Mongo.Collection('something') needs to run on both the server and client so we place it outside. (stated in the collection.js)
+
+"private" folder for server
+"public" folder for visitors
+"lib" folder are loaded first.
+
+meteor.startup(func) -> run code when a server or client starts
--- collections.js
+++ collections.js
@@ -1,2 +1,8 @@
 Incidents = new Mongo.Collection("incident");
 PSI = new Mongo.Collection("PSI");
+
+//mongo collection contructor creates a collection object which acts just like a MongoDb collection. 
+//if u pass a name, u are declaring a persistent collection -> one that is stored on the server and can be published to clients.
+//what u did above is to declare them as a global variable. 
+//to delclare them as a local variable, set the name to null.
+//meteor remove autopublish prevent the collections from publishing to your client. 
